8e87d881efccfef796935ccf703b861d21cdaca3
Moiz Chauhdry <moizchauhdry@gmail.com>
2025-05-31 02:25:57 +0500

    check frontend issue on live

--- routes/election.php
+++ routes/election.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Frontend\ElectionController;
 use App\Http\Controllers\Frontend\VoteController;
 use Illuminate\Support\Facades\Route;
 
-route::get('/election', [ElectionController::class, 'index'])->name('frontend.election.index');
+route::get('/bes/elections', [ElectionController::class, 'index'])->name('frontend.election.index');
 route::post('/submitVote', [VoteController::class, 'store'])->name('frontend.election.submitVote');
 route::post('/fetchSavedFingerTemplates', [ElectionController::class, 'fetchSavedFingerTemplates'])->name('frontend.election.fetchSavedFingerTemplates');
 route::post('/fetchVerifiedVoterData', [ElectionController::class, 'fetchVerifiedVoterData'])->name('frontend.election.fetchVerifiedVoterData');
